dcc901235f2bb3c34c3bba78968c8eca0b396b0a
Matthew Tran <Matthew.M.Tran-1@ou.edu>
2025-09-08 13:59:36 -0500

    Homework 1 Completed

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1a Sales Total
 
 // Your Name: Matthew Tran
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Did you seek help ? If yes, specify the helper or web link here: Visited office hours for GitHub help.
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,35 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            string item;
+
+            double count;
+            string countString;
+
+            double price;
+            string priceString;
+
+
+            const double taxrate = 0.085;
+
+
+            Console.WriteLine("What is the name of the product that you are buying?");
+            item = Console.ReadLine();
+
+            Console.WriteLine($"List how many {item}(s/ies) that you want to buy.");
+            countString = Console.ReadLine();
+            count = Convert.ToDouble(countString);
+
+            Console.WriteLine($"List the price for each {item}");
+            priceString = Console.ReadLine();
+            price = Convert.ToDouble(priceString);
+
+            double subtotal = price * count; //cost before tax, all var in section needs to be defined after inputs
+            double salestax = subtotal * taxrate;
+            double saletotal = salestax + subtotal; //total of tax and subtotal
+            Console.WriteLine("The subtotal for your purchase is " + subtotal.ToString("N2"));
+            Console.WriteLine("The tax for your purchase is " + salestax.ToString("N2"));
+            Console.WriteLine("The total cost for the sale is " + saletotal.ToString("N2"));
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1b Grade
 
 // Your Name: Matthew Tran
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Did you seek help ? If yes, specify the helper or web link here: Visited Office Hours for Github help.
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,58 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            string fname; //strings that stay strings
+            string lname;
+            string stuid;
+
+            double ex1; //needs next var set to get a value
+            double ex2;
+            double ex3;
+            double hw;
+            double clpar;
+
+            string ex1Str; //takes input from user to be converted later
+            string ex2Str;
+            string ex3Str;
+            string hwStr;
+            string clparStr;
+
+            Console.WriteLine("Type your first name:"); //takes inputs from user
+            fname = Console.ReadLine();
+            Console.WriteLine("Type your last name:");
+            lname = Console.ReadLine();
+            Console.WriteLine("Type your student id:");
+            stuid = Console.ReadLine();
+
+            Console.WriteLine("List your Exam 1 grade.");
+            ex1Str = Console.ReadLine();
+            ex1 = Convert.ToDouble(ex1Str);
+
+            Console.WriteLine("List your Exam 2 grade.");
+            ex2Str = Console.ReadLine();
+            ex2 = Convert.ToDouble(ex2Str);
+
+            Console.WriteLine("List your Exam 3 grade.");
+            ex3Str = Console.ReadLine();
+            ex3 = Convert.ToDouble(ex3Str);
+
+            Console.WriteLine("List your overall grade for the Homework section of the class.");
+            hwStr = Console.ReadLine();
+            hw = Convert.ToDouble(hwStr);
+
+            Console.WriteLine("List your overall grade for the Class Participation section of the class.");
+            clparStr = Console.ReadLine();
+            clpar = Convert.ToDouble(clparStr);
+
+            double ex1Weight = ex1 * 0.15; //takes input values from prompts and multiplies them by the syllabus weight (Fall2025)
+            double ex2Weight = ex2 * 0.25;
+            double ex3Weight = ex3 * 0.25;
+            double hwWeight = hw * 0.20;
+            double clparWeight = clpar * 0.15;
+            double sum = ex1Weight + ex2Weight + ex3Weight + hwWeight + clparWeight; //adds weighted portions to get final grade
+
+            Console.WriteLine($"{lname}, {fname} (ID:{stuid}), your final grade is: {sum.ToString("N2")}%");
+            Console.ReadKey();
         }
     }
 }
